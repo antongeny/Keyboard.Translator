0095171488c2106aeb5da263b0e3e86bddb4bd37
Antongeny <hang.anthony@gmail.com>
2023-03-18 00:39:59 -0400

    figured out how to hard code an object to display the variable

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ import './App.css'
 
 function App() {
 
-  let keyCombo = '';
-  const charA = " KC_A";
-
-  window.addEventListener('keydown', e => {
+  let keyCombo = { charA: "KC_A" }
+  // const charA = "KC_A";
+  // let displayedCode =[];
+  window.addEventListener('keyup', e => {
     keyCombo = '';
-
-    // console.log(e)
+    console.log(e);
     if(e.key === "a"){
-        keyCombo += charA;
-        console.log(keyCombo);
-        return keyCombo;
+      // displayedCode += keyCombo.charA;;
+      // console.log(keyCombo);
     }
+    // return displayedCode;
   })
   return (
     <div className="App">
         <h2>Press your key combination to program your keyboard</h2>
-        <h3>Your Key Combination is: {keyCombo} </h3>
+        <h3>Your Key Combination is:  </h3>
+        <div>{keyCombo.charA}</div>
     </div>
   )
 }
